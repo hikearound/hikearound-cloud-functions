1b683b0f628c70fc8310824b2462381961b72ab8
Patrick Dugan <dugan.pat@gmail.com>
2019-12-23 15:12:12 -0500

    Couple of nits

--- functions/emails/welcome.js
+++ functions/emails/welcome.js
@@ -1,15 +1,18 @@
-const FROM_EMAIL = 'noreply@hikearound.com';
 const TEMPLATE_ID = 'd-3541f72c3b33499bbd639a731bb72f94';
+const SUBJECT = 'Welcome to Hikearound';
 
-exports.handler = function(user, sgMail) {
+exports.handler = function(senderData, user, sgMail) {
     const msg = {
         to: user.email,
-        from: FROM_EMAIL,
+        from: {
+            name: senderData.name,
+            email: senderData.email,
+        },
         templateId: TEMPLATE_ID,
         dynamic_template_data: {
-            subject: 'Welcome to Hikearound',
+            subject: SUBJECT,
             name: user.displayName,
         },
     };
-    sgMail.send(msg);
+    return sgMail.send(msg);
 };
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const sgMail = require('@sendgrid/mail');
+
 const welcomeEmail = require('./emails/welcome');
+
+const senderData = {
+    name: 'Hikearound',
+    email: 'no-reply@tryhikearound.com',
+};
 
 admin.initializeApp();
 sgMail.setApiKey(functions.config().sendgrid.key);
 
 exports.welcomeEmail = functions.auth.user().onCreate((user) => {
-    welcomeEmail.handler(user, sgMail);
+    return welcomeEmail.handler(senderData, user, sgMail);
 });
