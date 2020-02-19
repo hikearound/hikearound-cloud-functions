9022fbd5718e08aa75c046c804493f29719efbb2
Patrick Dugan <dugan.pat@gmail.com>
2020-02-19 14:46:07 -0800

    One more for good measure

--- test/digest.test.js
+++ test/digest.test.js
@@ -39,12 +39,10 @@ describe('Every Friday at 9:00AM PST, Hikearound...', async () => {
 
         testData.hikeDescription =
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum erat arcu, posuere ac varius vel, viverra et lorem. Maecenas fringilla dignissim lobortis. Ut pharetra scelerisque eros, vitae pulvinar nisi pharetra quis.\n\nAliquam suscipit nec purus sit amet eleifend. Quisque quis turpis eget elit varius iaculis. Vivamus fermentum in quam eget vulputate. Aenean faucibus, ante nec fringilla faucibus, nunc ligula varius erat, non consequat elit diam vitae erat.';
-
         testData.hikeMapUrl =
             'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/images%2Fmaps%2FzvXj5WRBdxrlRTLm65SD.png?alt=media&token=aa122190-342e-431e-a342-ffa501dc0e3b';
 
         const db = authedApp({ uid: '1' });
-
         digest.digestEmail(admin, db, sgMail, testData);
     });
 });
