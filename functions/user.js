707fcbe6196b685a00079c686a7b7754085597ce
Patrick Dugan <dugan.pat@gmail.com>
2020-10-14 08:27:53 -0400

    Try this later

--- functions/user.js
+++ functions/user.js
@@ -1,4 +1,10 @@
 const admin = require('firebase-admin');
+const sentry = require('@sentry/node');
+const {
+    deleteUserDocuments,
+    deleteUserRecord,
+    deleteUserImages,
+} = require('../utils/user');
 
 const auth = admin.auth();
 
@@ -15,4 +21,15 @@ exports.updatePassword = async function (uid, password) {
         });
 
     return result;
+};
+
+exports.deleteUserData = async function (uid) {
+    try {
+        deleteUserDocuments(uid);
+        deleteUserRecord(uid);
+        deleteUserImages(uid);
+    } catch (e) {
+        sentry.captureException(e);
+    }
+    return false;
 };
--- utils/user.js
+++ utils/user.js
@@ -3,16 +3,34 @@ const search = require('../functions/search');
 
 const db = admin.firestore();
 const auth = admin.auth();
+const storage = admin.storage();
 
 exports.getUserData = async function (uid) {
     const userSnapshot = await db.collection('users').doc(uid).get();
     return userSnapshot.data();
 };
 
-exports.deleteUserData = async function (uid) {
-    db.collection('users').doc(uid).delete();
-    db.collection('favoritedHikes').doc(uid).delete();
-    return search.deleteUserRecord(uid);
+exports.deleteUserDocuments = async function (uid) {
+    await db.collection('users').doc(uid).delete();
+    await db.collection('favoritedHikes').doc(uid).delete();
+};
+
+exports.deleteUserRecord = async function (uid) {
+    await search.deleteUserRecord(uid);
+};
+
+exports.deleteUserImages = async function (uid) {
+    const userImages = [
+        `images/users/${uid}.jpg`,
+        `images/users/covers/${uid}_750x750.jpg`,
+        `images/users/thumbnails/${uid}_200x200.jpg`,
+    ];
+
+    userImages.map(async (image) => {
+        await storage.bucket().file(image).delete();
+    });
+
+    return true;
 };
 
 exports.getUserList = async function () {
