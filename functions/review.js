33a2107f9264401e6cd8509f86955156728a9513
Patrick Dugan <dugan.pat@gmail.com>
2020-11-11 20:02:33 -0500

    More nits

--- functions/review.js
+++ functions/review.js
@@ -1,32 +1,28 @@
 const { getReviewData, writeReviewData } = require('../utils/review');
 const { getHikeData } = require('../utils/hike');
 
+let average = 0;
+let count = 0;
+
 exports.addReviewToAverage = async function (rid) {
     const review = await getReviewData(rid);
     const hike = await getHikeData(review.hid);
-
-    let average = 0;
-    let count = 0;
 
     if (hike.review) {
         average = hike.review.average;
         count = hike.review.count;
     }
 
-    const reviewData = {
+    await writeReviewData(review.hid, {
         average: (count * average + review.rating) / (count + 1),
         count: count + 1,
-    };
-
-    await writeReviewData(review.hid, reviewData);
+    });
 };
 
 exports.updateAverageReview = async function (rid, snapshot) {
-    const originalReview = snapshot.before.data();
     const review = snapshot.after.data();
+    const originalReview = snapshot.before.data();
     const hike = await getHikeData(originalReview.hid);
-
-    let average = 0;
 
     if (hike.review.count > 1) {
         average =
@@ -37,19 +33,12 @@ exports.updateAverageReview = async function (rid, snapshot) {
     average =
         ((hike.review.count - 1) * average + review.rating) / hike.review.count;
 
-    const reviewData = {
-        average,
-        count: hike.review.count,
-    };
-
-    await writeReviewData(review.hid, reviewData);
+    await writeReviewData(review.hid, { average, count: hike.review.count });
 };
 
 exports.removeReviewFromAverage = async function (snapshot) {
     const review = snapshot.data();
     const hike = await getHikeData(review.hid);
-
-    let average = 0;
 
     if (hike.review.count > 1) {
         average =
@@ -57,10 +46,8 @@ exports.removeReviewFromAverage = async function (snapshot) {
             (hike.review.count - 1);
     }
 
-    const reviewData = {
+    await writeReviewData(review.hid, {
         average,
         count: hike.review.count - 1,
-    };
-
-    await writeReviewData(review.hid, reviewData);
+    });
 };
