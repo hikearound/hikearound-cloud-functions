36fef9b0d9e63de287df9d89a51a5a4b3ddfe919
Patrick Dugan <dugan.pat@gmail.com>
2021-05-12 02:14:48 -0700

    Package and prettier updates

--- constants/map.js
+++ constants/map.js
@@ -17,8 +17,7 @@ exports.config = {
     lineWidth: 2,
     poi: 0,
     logo: {
-        url:
-            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/images%2Fmisc%2Fstatic-map-marker.png?alt=media&token=12ecef53-98a3-4852-b280-be5eace88cf5',
+        url: 'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/images%2Fmisc%2Fstatic-map-marker.png?alt=media&token=12ecef53-98a3-4852-b280-be5eace88cf5',
         height: 35,
         width: 30,
     },
--- translations/en.js
+++ translations/en.js
@@ -9,8 +9,7 @@ exports.translations = {
     notif: {
         digest: {
             title: 'Check out this weeks best hikes',
-            body:
-                'Get ready for the weekend by checking out {{name}} and other hikes we think you might like',
+            body: 'Get ready for the weekend by checking out {{name}} and other hikes we think you might like',
         },
         reviewLike: {
             title: 'Liked by {{name}}',
@@ -20,19 +19,15 @@ exports.translations = {
     email: {
         common: {
             footer: {
-                intro:
-                    'This email was sent by Hikearound (542 Brannan St, San Francisco, CA 94107). ',
-                type:
-                    'To stop receiving {{type}} emails, <a class="text-link" href="{{url}}">Click here</a>. ',
-                global:
-                    'To stop receiving this and all other emails, <a class="text-link" href="{{url}}">Unsubscribe</a>.',
+                intro: 'This email was sent by Hikearound (542 Brannan St, San Francisco, CA 94107). ',
+                type: 'To stop receiving {{type}} emails, <a class="text-link" href="{{url}}">Click here</a>. ',
+                global: 'To stop receiving this and all other emails, <a class="text-link" href="{{url}}">Unsubscribe</a>.',
             },
         },
         digest: {
             subject:
                 'Get ready for the weekend by checking out {{name}} and other hikes we think you might like.',
-            body:
-                'Hi {{name}},<br>We recently added some hikes near {{location}} and this is one we think you might enjoy.',
+            body: 'Hi {{name}},<br>We recently added some hikes near {{location}} and this is one we think you might enjoy.',
             description:
                 '<a href="https://www.tryhikearound.com/hike/{{hid}}">{{name}}</a> is a hike located in {{city}}, {{state}}. It\'s a {{distance}} mile {{route}} with a net elevation gain of {{elevation}} feet.<p>{{description}} <a href="https://www.tryhikearound.com/hike/{{hid}}">Continue Reading</a></p>',
             cta: 'View Hike',
@@ -41,14 +36,12 @@ exports.translations = {
         },
         reset: {
             subject: 'Reset your Hikearound password.',
-            body:
-                'Hi {{name}},<br>Someone recently requested to change the password on your Hikearound account. If this was you, you can reset your password by clicking the button below.<p>If you no longer want to change your password, or if you didn\'t request this, please ignore and delete this message.<div class="signature">Thanks,</div>The Hikearound Team',
+            body: 'Hi {{name}},<br>Someone recently requested to change the password on your Hikearound account. If this was you, you can reset your password by clicking the button below.<p>If you no longer want to change your password, or if you didn\'t request this, please ignore and delete this message.<div class="signature">Thanks,</div>The Hikearound Team',
             cta: 'Reset Password',
         },
         welcome: {
             subject: '{{name}}, welcome to Hikearound!',
-            body:
-                'Hi {{name}},<br>We\'re stoked you decided to join Hikearound! Please verify your email address so that each week we can send you the best new hikes we think you\'ll enjoy.<div class="signature">Thanks,</div>The Hikearound Team',
+            body: 'Hi {{name}},<br>We\'re stoked you decided to join Hikearound! Please verify your email address so that each week we can send you the best new hikes we think you\'ll enjoy.<div class="signature">Thanks,</div>The Hikearound Team',
             cta: 'Confirm your email address',
         },
     },
